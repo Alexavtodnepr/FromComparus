dadfece83ec551e99fe1ec6a81a7050f68b9127a
Alexavtodnepr <82681866+Alexavtodnepr@users.noreply.github.com>
2021-12-14 02:54:21 +0200

    Add files via upload

--- src/app/modules/components/game/game.component.ts
+++ src/app/modules/components/game/game.component.ts
@@ -15,57 +15,89 @@ export class GameComponent implements OnInit, OnDestroy {
   public cells: number[] = new Array(10);
   public rows: number[] = new Array(10);
   public scoreUser: any[] = [];
+  public scoreComp: any[] = [];
   public item: Item | any;
   public theEnd: boolean = false;
   public winner: string = '';
   private subscription: Subscription | any;
-  private interval: number | any;
-  private showSpeedItems: number = 500;
+  private showSpeedItems: number = 400;
   private inputValue: number = 0;
 
-  constructor(public gameLogicService: GameLogicService) {}
-
-
+  constructor(public gameLogicService: GameLogicService) {
+  }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      delay: new FormControl('' || '1000')
+      delay: new FormControl('' || '1200')
     });
   }
 
-  startGameComponent(){
-    this.scoreUser.length = 0;
-    this.gameLogicService.scoreComp.length = 0;
-    this.gameLogicService.items = [];
-    clearInterval(this.interval);
-    this.theEnd = false;
-      const gameValue = +this.submit().delay;
-      console.log('Game has been started', gameValue + ' ms speed');
-      this.interval = setInterval(() =>{
-        this.gameLogicService.generateItems( 1, gameValue);
-        this.theEndGame();
-      }, this.showSpeedItems);
+  // добавляем красный цвет еще одним перебором с заданным интервалом пользователем
+  generateRedItems(timer: number){
+    this.gameLogicService.generateItems( 1);
+    this.gameLogicService.items.forEach((e)=>{
+      const redItemInterval = setInterval(()=>{
+        if(this.scoreComp.length == 10 || this.scoreUser.length == 10){
+          this.theEndGame();
+          clearInterval(redItemInterval);
+        }else if ( e.color === 'yellow' && e.click === false && this.scoreComp.length <= 10) {
+          e.color = 'red';
+          this.scoreComp.push(e);
+        }
+      }, timer)
+    });
   }
 
-  theEndGame(){
-    this.whoIsWinner();
-    if(this.scoreUser.length >= 10 || this.gameLogicService.scoreComp.length >= 10){
-      this.theEnd = true;
-      clearInterval(this.interval);
+//Обработка событий и добавление зеленых ячеек
+  clickThisItem(item: Item) {
+    if(item.color == 'red' || item.color === 'green' || this.scoreUser.length >= 10 || this.winner == 'Computer'){
+      return;
+    }else if ( item.color === 'yellow' && item.click === false) {
+      this.scoreUser.push(item);
+      item.color = 'green';
     }
   }
 
+//Старт игры
+  startGameComponent(){
+    this.theEnd = false;
+    this.scoreComp = [];
+    this.scoreUser = [];
+    this.gameLogicService.items = [];
+    this.winner = '';
+      const gameValue = +this.submit().delay;
+      console.log('Game has been started', gameValue + ' ms speed');
+    const generateInterval = setInterval(() =>{
+      if(!this.theEnd){
+        this.generateRedItems(gameValue);
+      }else{
+        clearInterval(generateInterval);
+      }
+    }, this.showSpeedItems);
+  }
+
+//Конец игры
+  theEndGame(){
+    if(this.scoreUser.length >= 10 || this.scoreComp.length >= 10) {
+      this.theEnd = true;
+      this.whoIsWinner();
+    }
+  }
+
+  //Определение победителя
   whoIsWinner(){
-    if(this.scoreUser.length > this.gameLogicService.scoreComp.length){
+    if(this.scoreUser.length > this.scoreComp.length){
       this.winner = 'User';
     }else{
       this.winner = 'Computer';
     }
   }
+
   submit() {
     return this.inputValue = {...this.form.value};
   }
 
+  //Установка стилей
   getStyles(item: Item){
     const top = (item.row * 55 - 50) + 'px';
     const left = (item.col * 55 - 50) + 'px';
@@ -74,15 +106,5 @@ export class GameComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
-  }
-
-  clickThisItem(item: Item) {
-    if ( item.color === 'yellow' && item.click === false) {
-      this.scoreUser.push(item);
-      item.color = 'green';
-    }
-    else if(item.color == 'red' || item.color === 'green'){
-      return
-    }
   }
 }
